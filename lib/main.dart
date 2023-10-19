50d76500191026f0925b210501a273c408c052ad
Duong147863 <duong147863@gmail.com>
2023-10-19 15:54:55 +0700

    sorecode

--- lib/main.dart
+++ lib/main.dart
@@ -71,44 +71,95 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Image.asset(
+          'root/assets/img/logo.png',
+          fit: BoxFit.contain,
+          height: 75,
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 50),
+            child: IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.only(top: 20),
+                        height: 300,
+                        width: 300,
+                        child: const CircleAvatar(
+                          backgroundImage:
+                              AssetImage('root/assets/img/avt.jpg'),
+                        ))
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width - 300,
+                      height: 100.0,
+                      child: Align(
+                          child: Column(
+                        children: const [
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(text: "Họ và Tên:"),
+                                TextSpan(text: "Nguyễn Bình Dương"),
+                              ],
+                            ),
+                          ),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(text: "Ngày Sinh:"),
+                                TextSpan(text: "15/04/2003"),
+                              ],
+                            ),
+                          ),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(text: "Giới Tính:"),
+                                TextSpan(text: "Nam"),
+                              ],
+                            ),
+                          ),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(text: "Số Điện Thoại:"),
+                                TextSpan(text: "12345678"),
+                              ],
+                            ),
+                          ),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(text: "Ngày Lập Thành Viên:"),
+                                TextSpan(text: "18/10/2023"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )),
+                    )
+                  ],
+                )
+              ],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
